b70a21333bdb88896a449c11b0119bceac64e6c2
Angelica <angelicapedroso05@gmail.com>
2021-08-31 00:44:07 -0300

    ex 3

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript-primeiros-passos/ex-3/script.js
@@ -0,0 +1,13 @@
+const a = 16;
+const b = 26;
+const c = 36;
+
+if (a > b && a > c) {
+    console.log('a é o maior');
+}
+else if (b > a && b > c) {
+    console.log('b é o maior');
+}
+else {
+    console.log('c é o maior');
+}
