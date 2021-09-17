1769f3eec90fd9907766c5aff33c4999538cfb00
Angelica <angelicapedroso05@gmail.com>
2021-09-17 01:47:15 -0300

    exercicio 1

---
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/script.js
@@ -0,0 +1,13 @@
+let state = document.getElementById('estado');
+
+let allStates = ['Acre (AC)', 'Alagoas (AL)', 'Amapá (AP)', 'Amazonas (AM)', 'Bahia (BA)', 'Ceará (CE)', 'Distrito Federal (DF)', 'Espírito Santo (ES)', 'Goiás (GO)', 'Maranhão (MA)', 'Mato Grosso (MT)', 'Mato Grosso do Sul (MS)', 'Minas Gerais (MG)', 'Pará (PA)', 'Paraíba (PB)', 'Paraná (PR)', 'Pernambuco (PE)', 'Piauí (PI)', 'Rio de Janeiro (RJ)', 'Rio Grande do Norte (RN)', 'Rio Grande do Sul (RS)', 'Rondônia (RO)', 'Roraima (RR)', 'Santa Catarina (SC)', 'São Paulo (SP)', 'Sergipe (SE)', 'Tocantins (TO)]'];
+
+function selectStates() {
+  for (let i = 0; i < allStates.length; i += 1) {
+    let createOption = document.createElement('option');
+    let stateSelected = allStates[i];
+    createOption.innerText = stateSelected;
+    state.appendChild(createOption)
+  }
+}
+selectStates();
