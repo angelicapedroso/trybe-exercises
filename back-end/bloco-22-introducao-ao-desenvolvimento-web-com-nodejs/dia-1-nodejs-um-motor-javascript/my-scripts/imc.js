a095c2b7ac5a48a4092363951b9e6b0334631028
angelicapedroso <angelicapedroso05@gmail.com>
2022-06-12 13:56:31 -0300

    feat: create function to rank imc

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
@@ -2,6 +2,15 @@ const readline = require('readline-sync');
 
 const peso = readline.question('Qual seu peso?');
 const altura = readline.questionFloat('Qual sua altura (em metros)?');
-const imc = peso / altura**2;
+const imc = peso / altura ** 2;
 
-console.log(imc);
+const getImc = (imc) => {
+  if(imc < 18.5) return 'abaixo do peso (magreza)';
+  else if(imc < 25) return 'peso normal';
+  else if(imc < 30) return 'acima do peso (sobrepeso)';
+  else if(imc < 35) return 'obesidade grau I';
+  else if(imc < 40) return 'obesidade grau II';
+  else return 'obesidade grau III';
+}
+
+console.log(`Seu IMC é de ${imc.toFixed(1)}, ${getImc(imc)}.`);
