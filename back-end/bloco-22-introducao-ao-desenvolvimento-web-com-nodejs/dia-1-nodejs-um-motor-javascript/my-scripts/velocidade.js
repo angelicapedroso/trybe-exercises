0cbb08985d61b7962dd2e3e08b42f69163a47583
angelicapedroso <angelicapedroso05@gmail.com>
2022-06-12 14:11:51 -0300

    feat: create script to calculate average speed

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/velocidade.js
@@ -0,0 +1,7 @@
+const readline = require('readline-sync');
+
+const distancia = readline.questionInt('Qual distância (em metros)?');
+const tempo = readline.questionInt('Qual o tempo (em segundos)');
+const vm = distancia / tempo;
+
+console.log(`A velocidade média é de ${vm.toFixed(1)} m/s.`);
