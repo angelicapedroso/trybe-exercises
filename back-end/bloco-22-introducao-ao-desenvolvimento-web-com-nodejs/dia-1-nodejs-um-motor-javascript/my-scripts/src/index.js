25f807131ad196b53abb2095ff7e6c8992224be8
angelicapedroso <angelicapedroso05@gmail.com>
2022-06-12 19:33:33 -0300

    feat: implement all scripts in main file

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/src/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/src/index.js
@@ -1,1 +1,22 @@
+const readline = require('readline-sync');
 
+const getSelectScript = () => {
+  const scriptList = [
+    `
+   1- calcular imc
+   2- calcular velocidade média
+   3- adivinhar número
+   `];
+
+  const question = readline.questionInt(`
+  Qual script deseja executar?
+   ${ scriptList }
+   Digite o número correspondente:`)
+
+  if (question === 1) return require('./imc');
+  else if (question === 2) return require('./velocidade');
+  else if (question === 3) return require('./sorteio');
+  else return console.log('número inválido');
+}
+
+getSelectScript();
