e89d48a91a483d0c4752c4d2f3184bb202a7d4a8
angelicapedroso <angelicapedroso05@gmail.com>
2022-06-16 16:45:50 -0300

    refactor: organize files

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercises/src/exercise-5/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercises/src/exercise-5/index.js
@@ -2,22 +2,22 @@ const fs = require('fs').promises;
 
 const arrToFile = async () => {
   const arr = ['Finalmente', 'estou', 'usando', 'Promise.all', '!!!'];
-  const createFiles = arr.map((el, index) => fs.writeFile(`./src/file${ index + 1 }.txt`, el));
+  const createFiles = arr.map((el, index) => fs.writeFile(`./src/exercise-6/file${ index + 1 }.txt`, el));
 
   await Promise.all(createFiles);
 
   const fileNames = [
-    './src/file1.txt',
-    './src/file2.txt',
-    './src/file3.txt',
-    './src/file4.txt',
-    './src/file5.txt',
+    './src/exercise-6/file1.txt',
+    './src/exercise-6/file2.txt',
+    './src/exercise-6/file3.txt',
+    './src/exercise-6/file4.txt',
+    './src/exercise-6/file5.txt',
   ];
 
   const readFiles = fileNames.map((fileName) => fs.readFile(fileName, 'utf-8'));
   const fileContents = await Promise.all(readFiles);
   const newFileContent = fileContents.join(' ');
-  await fs.writeFile('./src/fileAll.txt', newFileContent);
+  await fs.writeFile('./src/exercise-6/fileAll.txt', newFileContent);
 }
 
 arrToFile();
